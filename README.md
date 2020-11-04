7314932788aff9df09c32ead9618f5abecc2b6dc
Ryan Delaney <ryan@truss.works>
2020-11-04 17:22:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Factorio Drill Sound Effect
+
+Replaces the default sound effect for the repair pack with a better drill sound
+effect.
+
+Sound by Mike Koenig, found on
+[SoundBible](http://soundbible.com/1074-Drill.html). The sound file has a CC-BY
+3.0 license. Everything else here is released under the MIT license.
+
+I adapted the mod configuration from files in [Sonic Screwdriver
+mod](https://mods.factorio.com/mod/Sonic_Screw_Driver) by steinio. (MIT license).
---
+++ data.lua
@@ -0,0 +1,22 @@
+-- Change icon
+data.raw["repair-tool"]["repair-pack"]["icon"] = "__Drill_SoundBible__/repair-pack.png"
+data.raw["repair-tool"]["repair-pack"]["icon_size"] = 32
+
+-- Change repair sounds
+data.raw["utility-sounds"]["default"]["default_manual_repair"]["variations"][1]["filename"] = "__Drill_SoundBible__/manual-repair-simple.ogg"
+data.raw["utility-sounds"]["default"]["default_manual_repair"]["variations"][2] = nil
+for k, l in pairs(data.raw) do
+    for m, n in pairs(l) do
+        if n.repair_sound ~= nil then
+            if n.repair_sound.filename ~= nil then
+                if n.repair_sound.filename == "__base__/sound/manual-repair-simple.ogg" then 
+                    n.repair_sound.filename = "__Drill_SoundBible__/manual-repair-simple.ogg" 
+                end
+            end
+        end
+    end
+end
+
+-- Don't do wood
+table.insert(data.raw["container"]["wooden-chest"]["flags"], "not-repairable")
+table.insert(data.raw["electric-pole"]["small-electric-pole"]["flags"], "not-repairable")
